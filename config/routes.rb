5e2fed72a7bab195c188f41ab989313c8d7a1197
Marek Parafianowicz <marek.parafianowicz@gmail.com>
2016-11-29 15:49:07 +0100

    Create routing for teachers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   resources :students do
     get :subjects
   end
+
+  resources :teachers do
+    get :subjects
+  end
+
   devise_for :users
 
   resources :visitors, only: [:index]
+
+  resources :reports, only: [:subjects]
 end
