dc74ba09800237824ee477fe69769aba1284f6d4
Marek Parafianowicz <marek.parafianowicz@gmail.com>
2016-11-29 16:39:50 +0100

    Create routing for reports

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  
   resources :students do
     get :subjects
   end
-
   resources :teachers do
     get :subjects
   end
-
+  resources :visitors, only: [:index]
+  get 'subjects', to: 'reports#subjects', as: 'report_subcjects'
+  
   devise_for :users
 
-  resources :visitors, only: [:index]
-
-  resources :reports, only: [:subjects]
 end
